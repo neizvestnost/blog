71ae3c5371107997d51b1f2bb9daa93952e91373
Maks <neizvestnost@Makss-MacBook-Pro.local>
2024-04-29 21:42:17 +0300

    Fix bugs

--- app/javascript/controllers/contact_form_controller.js
+++ app/javascript/controllers/contact_form_controller.js
@@ -2,12 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   sendData() {
     const formData = new FormData(this.element);
+    const button = document.getElementById('contact_form--button');
+    button.disabled = false;
+    button.children[0].classList.remove('hidden');
+    button.classList.add('opacity-75');
     fetch('https://script.google.com/macros/s/AKfycbxbmMZEonqo2kjtN4aWXQTj0dfOxKsCJfJX-xMsP1WTU6CilSZl2HWjqWjtp5s8fb_-DQ/exec', { method: 'POST', body: formData })
-      .then(function(response) {
-          console.log('Success!', response)
+      .then(function() {
+        document.getElementById('contact_form_wrapper').classList.add('hidden')
+        document.getElementById('contact_form_thanks').classList.remove('hidden')
       })
-      .catch(function(error) {
-          console.error('Error!', error.message)
+      .catch(function() {
+        document.getElementById('contact_form_wrapper').classList.add('hidden')
+        document.getElementById('contact_form_error').classList.remove('hidden')
       })
   }
 }
